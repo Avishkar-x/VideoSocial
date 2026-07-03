803f797236c8ff8285b7483536e5c9bef73f7776
Avishkar Mali <avishkarmali01@gmail.com>
2026-07-03 18:32:57 +0530

    feat(comment): add CRUD operations for comments (testing pending)

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,24 +3,139 @@ import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
+import { Like } from "../models/like.model.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
+    
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
+    if(!mongoose.Types.ObjectId.isValid(videoId)) throw new ApiError(400, "Invalid video id");
 
+    if (page < 1 || limit < 1) {
+        throw new ApiError(400, "Invalid page or limit");
+    }
+
+    
+    const aggregate = Comment.aggregate([
+        {
+            $match:{
+                video: new mongoose.Types.ObjectId(videoId)
+            } 
+        },
+        {
+            $sort:{
+                createdAt:-1
+            }
+        },
+        {
+            $lookup:{
+                from:"users",
+                localField:"owner",
+                foreignField:"_id",
+                as:"owner",
+                pipeline:
+                [
+                    {
+                        $project:{
+                            fullName:1,
+                            username:1,
+                            avatar:1
+                        }
+                    }
+                ]
+            }
+            
+        },
+        {
+            $addFields:{
+                owner:{
+                    $first:"$owner"
+                }
+            }
+        }
+    ])
+
+    const options = {
+        page :Number(page),
+        limit:Number(limit)
+    }
+
+    const comments= await Comment.aggregatePaginate(aggregate,options)
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, comments, "Comments fetched successfully")
+    )
 })
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
+    
+    const {videoId} = req.params
+    if(!mongoose.Types.ObjectId.isValid(videoId)) throw new ApiError(400,"Invalid video Id")
+
+    const {content} = req.body
+    if(!content?.trim()) throw new ApiError(400,"Comment is required")
+    
+    const video = await Video.findById(videoId)
+
+    if(!video) throw new ApiError(404,"Video not found")
+
+    const comment = await Comment.create({
+        content,
+        // video:new mongoose.Types.ObjectId(videoId),
+        video:videoId,
+        owner: req.user._id
+    })
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,comment,"Comment created successfully")
+    )
 })
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
+    const {commentId} = req.params
+    if(!mongoose.Types.ObjectId.isValid(commentId)) throw new ApiError(400,"Invalid comment Id")
+    
+    const {content} = req.body
+    if(!content?.trim()) throw new ApiError(400,"Comment is required")
+    
+    const comment = await Comment.findById(commentId)
+    if(!comment) throw new ApiError(404,"comment not found")
+
+    if(!comment.owner.equals(req.user?._id)) throw new ApiError(403,"Only owner can update comment")
+
+    comment.content = content
+
+    await comment.save({validateBeforeSave:false})
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,comment,"Comment updated successfully")
+    )
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
+    const {commentId} = req.params
+    if(!mongoose.Types.ObjectId.isValid(commentId)) throw new ApiError(400,"Invalid comment Id")
+
+    const comment = await Comment.findById(commentId)
+    if(!comment) throw new ApiError(404,"comment not found")
+
+    if(!comment.owner.equals(req.user?._id)) throw new ApiError(403,"Only owner can delete comment")
+    
+    await Like.deleteMany({
+        comment: comment._id
+    })  ;
+    await comment.deleteOne()
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,{},"Comment deleted successfully"))
 })
 
 export {
