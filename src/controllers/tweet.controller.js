1fcbcb7c28dfafceab843996e35d884ba6347ee9
Avishkar Mali <avishkarmali01@gmail.com>
2026-07-05 12:19:54 +0530

    test(tweet): verify tweet controllers and fix discovered bugs

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Like } from "../models/like.model.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     const {content} = req.body
