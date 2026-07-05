5810f46dc56abbc78ce67d5b11997e2bb49de0f4
Avishkar Mali <avishkarmali01@gmail.com>
2026-07-05 12:01:39 +0530

    feat(tweet): implement all tweet controllers (testing pending)

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -6,19 +6,92 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
+    const {content} = req.body
+
+    if(!content.trim()) 
+        throw new ApiError(400,"Tweet content missing")
+
+    const tweet = await Tweet.create({
+        content:content.trim(),
+        owner:req.user._id
+    })
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,tweet,"Tweet created successfully")
+    )
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
+    const {userId} = req.params
+    if(!mongoose.Types.ObjectId.isValid(userId))
+        throw new ApiError(400,"Invalid user id")
+
+    const tweets = await Tweet.find({
+        owner: userId
+    }).sort({ createdAt: -1 })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,tweets,"User tweets fetched successfully")
+    )
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
+    const {content} = req.body
+    const {tweetId} = req.params
+    if(!content?.trim())
+        throw new ApiError(400,"Content required to update tweet")
+
+    if(!mongoose.Types.ObjectId.isValid(tweetId))
+        throw new ApiError(400,"Invalid tweet id")
+
+    const tweet = await Tweet.findById(tweetId)
+    if(!tweet) 
+        throw new ApiError(404, "Tweet was not found")
+
+    if(!req.user._id.equals(tweet.owner))
+        throw new ApiError(403,"Only owners can update tweet")
+
+    if(tweet.content === content.trim())
+        throw new ApiError(403,"Tweet content is unchanged")
+
+    tweet.content = content.trim()
+
+    await tweet.save()
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,tweet,"Tweet updated successfully")
+    )
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
+    const {tweetId} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(tweetId))
+        throw new ApiError(400,"Invalid tweet id")
+
+    const tweet = await Tweet.findById(tweetId)
+    if(!tweet) 
+        throw new ApiError(404, "Tweet was not found")
+
+    if(!req.user._id.equals(tweet.owner))
+        throw new ApiError(403,"Only owners can delete tweet")
+
+    await Like.deleteMany({
+        tweet: tweet._id
+    })
+    await tweet.deleteOne()
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,{},"Tweet deleted successfully")
+    )
 })
 
 export {
